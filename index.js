183554a12ba7f1cb744c9010c8dfa1aa1e5675e8
amanjais123 <162698463+amanjais123@users.noreply.github.com>
2025-06-18 01:00:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,60 @@
-const express = require("express") ;
-const app = express() ;
+const express = require("express");
+const app = express();
 
-const userRoutes = require("./routes/User") ;
-const profileRoutes = require("./routes/Profile") ;
-const paymentRoutes = require("./routes/Payment") ;
-const courseRoutes = require("./routes/Course") ;
-const contactRoutes = require("./routes/Contact") ;
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payment");
+const courseRoutes = require("./routes/Course");
 
-const database = require("./config/database") ;
-const cookieParser = require("cookie-parser") ;
-const cors = require("cors") ;
-const {cloudinaryConnect} =  require("./config/cloudinary") ;
-const fileUpload = require("express-fileupload") ;
-const dotenv = require("dotenv") ;
 
-const PORT = process.env.PORT || 4000 ;
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const {cloudinaryConnect } = require("./config/clodinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
 
-dotenv.config() ;
-app.use(fileUpload({
-    useTempFiles : true ,
-    tempFileDir : "./tmp" ,
-}));
-app.use(express.json()) ;
-app.use(cookieParser()) ; 
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+//database connect
+database.connect();
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
 app.use(
-    cors({
-        origin :"http://localhost:3000" ,
-        credentials : true,
-    })
+	cors({
+		origin:"http://localhost:3000",
+		credentials:true,
+	})
 )
 
+app.use(
+	fileUpload({
+		useTempFiles:true,
+		tempFileDir:"/tmp",
+	})
+)
+//cloudinary connection
+cloudinaryConnect();
+
+//routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
 
 
-cloudinaryConnect() ;
 
+//def route
 
-app.use("/api/v1/auth", userRoutes) ;
-app.use("/api/v1/profile", profileRoutes) ;
-app.use("/api/v1/course", courseRoutes) ;
-app.use("/api/v1/payment", paymentRoutes) ;
-
-app.get("/" , (req , res) =>{
-    return res.json({
-        success : true ,
-        message : "your server iis up and running......" ,
-    });
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running....'
+	});
 });
 
-
-const {connect} = require('./config/database');
-connect() ;
-
-
-
-
-app.listen(PORT , ()=>{
-    console.log('App is running') ;
-})  ;  
+app.listen(PORT, () => {
+	console.log(`App is running at ${PORT}`)
+})
