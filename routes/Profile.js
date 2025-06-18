0a9758a2dc7815401ce5051a17bcfd1b94dd7a20
amanjais123 <162698463+amanjais123@users.noreply.github.com>
2025-06-18 21:53:23 +0530

    Update Profile.js

--- routes/Profile.js
+++ routes/Profile.js
@@ -11,16 +11,13 @@ getEnrolledCourses
   
 } = require("../controllers/Profile")
 console.log("🧠 profileRoutes loaded");
-// ********************************************************************************************************
-//                                      Profile routes
-// ********************************************************************************************************
-// Delet User Account
+
 
 router.delete("/deleteProfile",auth, deleteAccount)
 router.put("/updateProfile", auth, updateprofile)
 router.get("/getUserDetails", auth, getAllUserDetails)
 // Get Enrolled Courses
-router.get("/getEnrolledCourses", getEnrolledCourses)
+router.get("/getEnrolledCourses",auth,isStudent, getEnrolledCourses)
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
 router.get("/instructor", auth,isInstructor, instructorDashboard)
 
