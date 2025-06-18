ab3deeff17ed44a3e879725a2e9810c444a97223
amanjais123 <162698463+amanjais123@users.noreply.github.com>
2025-06-18 11:04:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://edunova-edtech.vercel.app/",
 		credentials:true,
 	})
 )
