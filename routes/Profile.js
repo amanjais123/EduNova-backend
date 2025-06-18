6ab6465fd2953c7915813395071a2ec8a399db26
amanjais123 <162698463+amanjais123@users.noreply.github.com>
2025-06-18 17:10:33 +0530

    Update Profile.js

--- routes/Profile.js
+++ routes/Profile.js
@@ -20,7 +20,7 @@ router.delete("/deleteProfile",auth, deleteAccount)
 router.put("/updateProfile", auth, updateprofile)
 router.get("/getUserDetails", auth, getAllUserDetails)
 // Get Enrolled Courses
-router.get("/getEnrolledCourses", auth , isStudent, getEnrolledCourses)
+router.get("/getEnrolledCourses", isStudent, getEnrolledCourses)
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
 router.get("/instructor", auth,isInstructor, instructorDashboard)
 
