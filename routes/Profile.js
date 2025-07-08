281ca71721c75eb0a72684d1b4f220b482441eda
amanjais123 <162698463+amanjais123@users.noreply.github.com>
2025-07-08 12:23:21 +0530

    Update Profile.js

--- routes/Profile.js
+++ routes/Profile.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const router = express.Router()
+
+
 const { auth, isStudent , isInstructor} = require("../middleware/auth")
 const {
   deleteAccount,
@@ -10,15 +12,17 @@ instructorDashboard,
 getEnrolledCourses
   
 } = require("../controllers/Profile")
-console.log("🧠 profileRoutes loaded");
+console.log("profileRoutes me aa gye");
 
 
 router.delete("/deleteProfile",auth, deleteAccount)
 router.put("/updateProfile", auth, updateprofile)
 router.get("/getUserDetails", auth, getAllUserDetails)
-// Get Enrolled Courses
+
 router.get("/getEnrolledCourses",auth,isStudent, getEnrolledCourses)
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
 router.get("/instructor", auth,isInstructor, instructorDashboard)
+
+
 
 module.exports = router ;
